674203788df66cd9d7523723e0565dfbb197ae6b
Steven Pousty <spousty@redhat.com>
2014-10-26 11:35:07 -0700

    bring it up to date

---
+++ src/main/java/com/openshift/ff/ws/UserWS.java
@@ -0,0 +1,18 @@
+package com.openshift.ff.ws;
+
+import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ * Created by spousty on 10/24/14.
+ */
+
+@RequestScoped
+@Stateless
+@Path("/")
+public class UserWS {
+
+
+
+}
